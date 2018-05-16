a8e83976f2d3484d7e5d36cd7bfde95e6e7303e2
podshibyakina <39329431+podshibyakina@users.noreply.github.com>
2018-05-16 09:02:09 +0000

    Update division.py
    change division

--- division.py
+++ division.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 import argparse
-import io
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         'Divide one number by another')
     parser.add_argument('numerator', type=float, help='Numerator')
+    print("Hello World!")
     parser.add_argument('divisor', type=float, help='Divisor')
     
     args = parser.parse_args()
-    try:
+   
     print(args.numerator / args.divisor)
-    exept:
-        raise io.RumTimeError
+    
